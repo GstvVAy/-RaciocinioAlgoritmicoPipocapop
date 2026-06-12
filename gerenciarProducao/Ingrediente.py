7f6f5077af0eb383383cf23b0cf92b5911ee5ff3
GstvVAy <111517248+GstvVAy@users.noreply.github.com>
2026-06-12 09:04:04 -0300

    Update Ingrediente.py

--- gerenciarProducao/Ingrediente.py
+++ gerenciarProducao/Ingrediente.py
@@ -1,1 +1,10 @@
-ingredientes ={}
+ingredientes = {}
+
+def add_ingrediente(nome: str, quantidade: float, unidade: str = "un"):
+	ingredientes[nome] = {"quantidade": quantidade, "unidade": unidade}
+
+def get_ingrediente(nome: str):
+	return ingredientes.get(nome)
+
+def list_ingredientes():
+	return ingredientes
