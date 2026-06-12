685f7eaa3c2bf5aa638c874d7823e28be365c892
GstvVAy <111517248+GstvVAy@users.noreply.github.com>
2026-06-12 09:04:53 -0300

    Update Produto.py

--- gerenciarProducao/Produto.py
+++ gerenciarProducao/Produto.py
@@ -1,5 +1,9 @@
-
-
-
 class Produto:
-    ingredietes
+    def __init__(self, id_produto: str, nome: str, ingredientes: dict, preco: float = 0.0):
+        self.id = id_produto
+        self.nome = nome
+        self.ingredientes = ingredientes
+        self.preco = preco
+
+    def as_dict(self):
+        return {"id": self.id, "nome": self.nome, "ingredientes": self.ingredientes, "preco": self.preco}
